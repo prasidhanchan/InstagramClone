015ab3df0f64c4b550aeee682dd4f19f2a2b9743
Prasidh Gopal Anchan <prasidhpoojary22@gmail.com>
2024-02-06 20:13:07 +0530

    Minor Improvements and Bug Fixes

--- core/ui/src/main/java/com/instagramclone/ui/components/PostCard.kt
+++ core/ui/src/main/java/com/instagramclone/ui/components/PostCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,7 +19,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -47,11 +51,12 @@ fun PostCard(
     onUsernameClicked: () -> Unit
 ) {
     val interactionSource = remember { MutableInteractionSource() }
+    var imageSize by remember { mutableFloatStateOf(0f) }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight(Alignment.CenterVertically)
-            .padding(bottom = 5.dp),
+            .padding(bottom = 15.dp),
         color = Utils.IgBlack
     ) {
         Column(
@@ -103,10 +108,12 @@ fun PostCard(
                 }
             }
 
+
             AsyncImage(
-                modifier = Modifier,
+                modifier = Modifier.defaultMinSize(minHeight = 200.dp),
                 model = post.images.first(), //TODO integrate Pager
-                contentScale = ContentScale.Crop,
+                contentScale = ContentScale.Fit,
+                onLoading = { imageSize = it.painter?.intrinsicSize?.height ?: 0f },
                 contentDescription = stringResource(R.string.post, post.username)
             )
 
