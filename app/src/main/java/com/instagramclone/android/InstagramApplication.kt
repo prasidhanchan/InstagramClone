79f1b66b520f1133f0df449fb03d83adc0150cbb
Prasidh Gopal Anchan <prasidhpoojary22@gmail.com>
2024-05-01 20:31:05 +0530

    feat: Added coil VideoFrameDecoder component to render video frame

--- app/src/main/java/com/instagramclone/android/InstagramApplication.kt
+++ app/src/main/java/com/instagramclone/android/InstagramApplication.kt
@@ -1,7 +1,18 @@
 package com.instagramclone.android
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.decode.VideoFrameDecoder
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class InstagramApplication : Application()
+class InstagramApplication : Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .components {
+                add(VideoFrameDecoder.Factory())
+            }
+            .build()
+    }
+}
