cda4efbf9d6629146aa2797905e519ff317dfbae
Prasidh Gopal Anchan <prasidhgopalanchan@gmail.com>
2024-07-04 22:22:27 +0530

    chore: bump dependencies

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -2,5 +2,5 @@ object AndroidConfig {
     const val compileSdk = 34
     const val minSdk = 29
     const val targetSdk = 34
-    const val kotlinCompilerExtensionVersion = "1.5.12"
+    const val kotlinCompilerExtensionVersion = "1.5.14"
 }
